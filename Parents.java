8181468781f47ca3ab1b3a72b2e7b8b481b09c96
benjacot <jacotb@wwu.edu>
2019-02-22 20:57:19 -0800

    Here I am!

---
+++ Parents.java
@@ -0,0 +1,11 @@
+class Parents {
+
+	public static void main(String[] args) {
+		System.out.println("Hey Dad!");
+		System.out.println("Hey mom!");
+		System.out.println("This is Ben");
+		System.out.println("3 plus 4 equals");
+		int val = 3 + 4;
+		System.out.println(val);
+	}
+}
